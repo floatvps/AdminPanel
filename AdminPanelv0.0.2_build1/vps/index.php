08ac3943f87937bb89ef036055ea06568ec1c926
samdisk11 <thexkey38@gmail.com>
2019-02-26 11:44:25 -0600

    Add files via upload

---
+++ AdminPanelv0.0.2_build1/vps/index.php
@@ -0,0 +1,97 @@
+<?php 
+	include('functions.php');
+
+	if (!isLoggedIn()) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<script type='text/javascript'> 
+title = " [AdminPanel - Home] ";
+position = 0;
+function scrolltitle() {
+    document.title = title.substring(position, title.length) + title.substring(0, position); 
+    position++;
+    if (position > title.length) position = 0;
+    titleScroll = window.setTimeout(scrolltitle,170);
+}
+scrolltitle();
+</script>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="en">
+<link href="style.css" rel="stylesheet" type="text/css"></head><body>
+    <div id="page">         
+        <div id="header"> <a href="#"><img id="logo" src="images/logo.jpg" alt=""></a>
+            <h1><a href="#">AdminPanel v0.0.2</a></h1>
+            <p class="subtitle">You really shouldnt be here.</p>
+        </div>	
+        <div id="menu">
+            <ul><li><a href="../">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Your VPS</a></li>
+                <li><a href="#">Donate</a></li>
+                <li><a href="#">Discord</a></li>
+            </ul></div>
+			
+        <div id="main"><div id="main2">	
+                <div id="content">
+                    <h2>Login</h2>
+                   <form method="post" action="login.php">
+
+		           <?php echo display_error(); ?>
+
+		         <!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+		<!-- logged in user information -->
+		<div class="profile_info">
+			<img src="images/user_profile.png"  >
+
+			<div>
+				<?php  if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+					<small>
+						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+						<br>
+						<a href="index.php?logout='1'" style="color: red;">logout</a>
+					</small>
+					
+
+				<?php endif ?>
+			</div>
+		</div>
+	               </form>
+                   <h2>About AdminPanel</h2>
+                    <div class="box">
+                        <p>AdminPanel was created because there were no free or paid ESX-i Control Panels and the only one was AutoVM. Upon testing AutoVM, it proved to be super broken and buggy. i created AdminPanel because:</p>
+                        <ul><li><span>+</span>I wanted something that WORKED</li>
+                            <li><span>+</span>I wanted a Control panel for FloatVPS.</li>
+                            <li><span>+</span>I also want to learn the ESX-i API</li>
+                            <li><span>+</span>Did i mention its Open Source?</li>
+                            <li><span>+</span>Its also 100% Free Forever!</li>
+                        </ul><p>AdminPanel is free to use. Please turn off adblock and do not remove the ads! i spent lots of time coding this panel!</p>
+                    </div>
+                  
+                </div><!-- content -->   
+                                
+                <div class="clearing"> </div>
+        </div></div><!-- main --><!-- main2 -->
+        <div id="footer">
+            <p>Copyright © 2011, designed by <a href="http://www.alphastudio.pl/" target="_blank">Alpha Studio</a> | AdminPanel - by FloatVPS/Samdisk - <a href="" target="_blank">Github</a>
+</p>
+        </div>
+    </div><!-- page -->
+</body></html>
---
+++ AdminPanelv0.0.2_build1/vps/register.php
@@ -0,0 +1,83 @@
+<?php include('functions.php') ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<script type='text/javascript'> 
+title = " [AdminPanel - Sign Up] ";
+position = 0;
+function scrolltitle() {
+    document.title = title.substring(position, title.length) + title.substring(0, position); 
+    position++;
+    if (position > title.length) position = 0;
+    titleScroll = window.setTimeout(scrolltitle,170);
+}
+scrolltitle();
+</script>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="en">
+<link href="style.css" rel="stylesheet" type="text/css"></head><body>
+    <div id="page">         
+        <div id="header"> <a href="#"><img id="logo" src="images/logo.jpg" alt=""></a>
+            <h1><a href="#">AdminPanel v0.0.2</a></h1>
+            <p class="subtitle">You really shouldnt be here.</p>
+        </div>	
+        <div id="menu">
+            <ul><li><a href="../">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Your VPS</a></li>
+                <li><a href="#">Donate</a></li>
+                <li><a href="#">Discord</a></li>
+            </ul></div>
+			
+        <div id="main"><div id="main2">	
+                <div id="content">
+                    <h2>Login</h2>
+                  <form method="post" action="register.php">
+
+		<?php echo display_error(); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" value="<?php echo $username; ?>">
+		</div>
+		<div class="input-group">
+			<label>Discord/Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="register_btn">Register</button>
+		</div>
+		<p>
+			Already a member? <a href="login.php">Sign in</a>
+		</p>
+	</form>
+                   <h2>About AdminPanel</h2>
+                    <div class="box">
+                        <p>AdminPanel was created because there were no free or paid ESX-i Control Panels and the only one was AutoVM. Upon testing AutoVM, it proved to be super broken and buggy. i created AdminPanel because:</p>
+                        <ul><li><span>+</span>I wanted something that WORKED</li>
+                            <li><span>+</span>I wanted a Control panel for FloatVPS.</li>
+                            <li><span>+</span>I also want to learn the ESX-i API</li>
+                            <li><span>+</span>Did i mention its Open Source?</li>
+                            <li><span>+</span>Its also 100% Free Forever!</li>
+                        </ul><p>AdminPanel is free to use. Please turn off adblock and do not remove the ads! i spent lots of time coding this panel!</p>
+                    </div>
+                  
+                </div><!-- content -->   
+                                
+                <div class="clearing"> </div>
+        </div></div><!-- main --><!-- main2 -->
+        <div id="footer">
+            <p>Copyright © 2011, designed by <a href="http://www.alphastudio.pl/" target="_blank">Alpha Studio</a> | AdminPanel - by FloatVPS/Samdisk - <a href="" target="_blank">Github</a>
+</p>
+        </div>
+    </div><!-- page -->
+</body></html>
